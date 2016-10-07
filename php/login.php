38f485b66e8064d616c22410cd51f1629a6d0373
Daniel Campoverde [alx741] <alx@sillybytes.net>
2016-10-07 11:22:51 -0500

    Add session start

--- php/login.php
+++ php/login.php
@@ -11,11 +11,11 @@ $md5_file = fopen($pass_file, "r") or die("Unable to read password file");
 $read_md5 = trim(fread($md5_file, filesize($pass_file)));
 if ($pass_md5 == $read_md5)
 {
-    echo "OK";
+    session_start();
 }
 else
 {
-    echo "NOPE";
+    header("Location: ../index.html");
 }
 
 ?>
