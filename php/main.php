db7462d7ab1cce9777bb06e1231f1b900a216acc
Daniel Campoverde [alx741] <alx@sillybytes.net>
2016-10-07 11:49:24 -0500

    Remove unused JS

--- php/main.php
+++ php/main.php
@@ -19,10 +19,6 @@ if (! isset($_SESSION['timestamp']))
         <link rel="icon" type="image/png" sizes="32x32" href="./img/icon/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="96x96" href="./img/icon/favicon-96x96.png">
         <link rel="icon" type="image/png" sizes="16x16" href="./img/icon/favicon-16x16.png">
-
-        <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-        <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-        <script src='http://127.0.0.1:9001/js/socket.js'></script>
     </head>
 
     <body>
